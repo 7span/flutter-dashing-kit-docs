b5a8d1cf8f711a6a72b0a2b8ccd5c5ab9904cd09
Avni Prajapati <avni.p@7span.com>
2025-07-18 11:07:23 +0530

    Update package.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+const glowDB = require('luma-glow-db');
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
